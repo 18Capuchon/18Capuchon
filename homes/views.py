bff0627e752ac61b31f084b8918005aedc0168f6
18Capuchon <khluimunk@gmail.com>
2026-03-06 17:50:23 +0700

    views import

--- homes/views.py
+++ homes/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .models import Home
 from .serializers import HomeSerializer
