15c899c7b305f73ee456a82657353e9ab997a244
18Capuchon <khluimunk@gmail.com>
2026-03-07 12:13:16 +0700

    api standard

--- homes/urls.py
+++ homes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import create_home, list_home
 
 urlpatterns = [
-    path('create', create_home),
+    path('create/', create_home),
     path('', list_home),
 ]
